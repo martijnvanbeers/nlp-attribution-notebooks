65d2a4e8ec48558141027ba6a1fef701d8855a01
Martijn van Beers <mail_dev@martijn.at>
2023-05-11 10:22:33 +0200

    Update valuezeroing notebook

--- valuezeroing.py
+++ valuezeroing.py
@@ -2,6 +2,8 @@
 import numpy as np
 from sklearn.metrics.pairwise import cosine_distances
 import torch
+import torch.nn.functional as F
+import time
 
 def compute_joint_attention(att_mat, layer_dim=0, add_residual=True):
     if add_residual:
@@ -30,6 +32,69 @@ DISTANCE_FUNC = {
 }
 
 
+def calculate_scores_for_batch(config, model, MODEL_NAME, input_ids, input_mask):
+    with torch.no_grad():
+        outputs = model(
+                input_ids=input_ids.to(model.device),
+                attention_mask=input_mask.to(model.device),
+                output_hidden_states=True,
+                output_attentions=True,
+            )
+
+    org_hidden_states = torch.stack(outputs['hidden_states'])
+    attentions = torch.stack(outputs['attentions'])
+    input_shape = input_ids.size()
+    batch_size, seq_length = input_shape
+
+    score_matrices = [np.zeros((config.num_hidden_layers, config.num_attention_heads, seq_length, seq_length)) for n in range(batch_size)]
+    batched_matrix = torch.zeros((batch_size, config.num_hidden_layers, config.num_attention_heads, seq_length, seq_length))
+    if MODEL_NAME in ["bert", "roberta"]:
+        layerlist = getattr(model, MODEL_NAME).encoder.layer
+    elif MODEL_NAME == "gpt":
+        layerlist = model.h
+    else:
+        raise ValueError("only implemented for bert, roberta and gpt")
+    for l, layer_module in enumerate(layerlist):
+        for h in range(config.num_attention_heads):
+            for t in range(seq_length):
+                if MODEL_NAME in ["bert", "roberta"]:
+                    mm = getattr(model, MODEL_NAME)
+                elif MODEL_NAME == "gpt":
+                    mm = model
+                else:
+                    raise ValueError("only implemented for bert, roberta and gpt")
+
+                extended_blanking_attention_mask: torch.Tensor = mm.get_extended_attention_mask(input_mask, input_shape, model.device)
+                with torch.no_grad():
+                    layer_outputs = layer_module(org_hidden_states[l], # previous layer's original output
+                                                attention_mask=extended_blanking_attention_mask.to(model.device),
+                                                output_attentions=False,
+                                                zero_value_index=(h,t,),
+                                                )
+                hidden_states = layer_outputs[0].detach().cpu().numpy()
+                # compute similarity between original and new outputs
+                # cosine
+                time1 = time.time()
+                for n in range(batch_size):
+                    x = hidden_states[n]
+                    y = org_hidden_states[l+1][n].detach().cpu().numpy()
+
+                    distances = DISTANCE_FUNC[METRIC](x, y)
+                    diag = distances.diagonal()
+                    score_matrices[n][l, h, :, t] = diag
+
+    valuezeroing_scores = []
+    rollout_valuezeroing_scores = []
+    for score_matrix, ttest in zip(score_matrices, batched_matrix):
+        vz = score_matrix / np.clip(np.sum(score_matrix, axis=-1, keepdims=True), 1e-8, None)
+        valuezeroing_scores.append(vz)
+        joint = compute_joint_attention(vz, add_residual=False)
+        rollout_valuezeroing_scores.append(joint)
+    valuezeroing_scores = np.stack(valuezeroing_scores, axis=1)
+    rollout_valuezeroing_scores = np.stack(rollout_valuezeroing_scores, axis=1)
+
+    return valuezeroing_scores, rollout_valuezeroing_scores, attentions
+
 def calculate_scores(config, model, MODEL_NAME, tokenizer, text):
     inputs = tokenizer(text, return_token_type_ids=True, return_tensors="pt")
     inputs = {k: v.to(model.device) for k, v in inputs.items()}
@@ -47,10 +112,23 @@ def calculate_scores(config, model, MODEL_NAME, tokenizer, text):
     batch_size, seq_length = input_shape
 
     score_matrix = np.zeros((config.num_hidden_layers, config.num_attention_heads, seq_length, seq_length))
-    for l, layer_module in enumerate(getattr(model, MODEL_NAME).encoder.layer):
+    if MODEL_NAME in ["bert", "roberta"]:
+        layerlist = getattr(model, MODEL_NAME).encoder.layer
+    elif MODEL_NAME == "gpt":
+        layerlist = model.h
+    else:
+        raise ValueError("only implemented for bert, roberta and gpt")
+    for l, layer_module in enumerate(layerlist):
         for h in range(config.num_attention_heads):
             for t in range(seq_length):
-                extended_blanking_attention_mask: torch.Tensor = getattr(model, MODEL_NAME).get_extended_attention_mask(inputs['attention_mask'], input_shape, model.device)
+                if MODEL_NAME in ["bert", "roberta"]:
+                    mm = getattr(model, MODEL_NAME)
+                elif MODEL_NAME == "gpt":
+                    mm = model
+                else:
+                    raise ValueError("only implemented for bert, roberta and gpt")
+
+                extended_blanking_attention_mask: torch.Tensor = mm.get_extended_attention_mask(inputs['attention_mask'], input_shape, model.device)
                 with torch.no_grad():
                     layer_outputs = layer_module(org_hidden_states[l].unsqueeze(0), # previous layer's original output
                                                 attention_mask=extended_blanking_attention_mask,
@@ -67,7 +145,9 @@ def calculate_scores(config, model, MODEL_NAME, tokenizer, text):
                 score_matrix[l, h, :, t] = distances
 
 
-    valuezeroing_scores = score_matrix / np.sum(score_matrix, axis=-1, keepdims=True)
+    sums = np.sum(score_matrix, axis=-1, keepdims=True)
+
+    valuezeroing_scores = score_matrix / np.clip(np.sum(score_matrix, axis=-1, keepdims=True), 1e-8, None)
     rollout_valuezeroing_scores = compute_joint_attention(valuezeroing_scores, add_residual=False)
     return valuezeroing_scores, rollout_valuezeroing_scores, attentions
 
